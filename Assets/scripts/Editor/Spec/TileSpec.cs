3b80afc6e81a6e1065d4ba37edcca166ddae2df8
Anthony Luu <anthony@axiomzen.co>
2016-04-22 14:27:49 -0700

    got unit tests to work

---
+++ Assets/scripts/Editor/Spec/TileSpec.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TurnBasedStrategyGame {
+
+	[TestFixture]
+	[Category("Sample Tests")]
+	public class TileSpec {
+
+		[Test]
+		public void getTopLeftCornerNeighbours() {
+			List<List<TileModel>> map = new List<List<TileModel>>();
+			// generate map
+			for (int i = 0; i < 10; i++) {
+				List<TileModel> row = new List<TileModel>();
+				for (int j = 0; j < 10; j++) {
+					TileModel myTile = new TileModel (new Vector2(i,j));
+					row.Add (myTile);
+				}
+				map.Add (row);
+			}
+
+			// get top left tile
+			TileModel topLeftTile = map[0][0];
+
+			// assert
+			topLeftTile.generateNeighbours(map);
+			List<TileModel> actualNeighbours = topLeftTile.neighbours;
+			Assert.IsTrue (actualNeighbours.Count == 2);
+		}
+	}
+}
---
+++ Assets/scripts/Models/TileModel.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TurnBasedStrategyGame {
+	public class TileModel {
+		public Vector2 gridPosition = Vector2.zero;
+
+		public int movementCost = 1;
+		public bool impassible = false;
+		public bool occupied = false;
+		public List<TileModel> neighbours = new List<TileModel>();
+
+		public TileModel(Vector2 gridPos) {
+			this.gridPosition = gridPos;
+		}
+
+		// neighbours is a List of Tiles adjacent to this Tile
+		public void generateNeighbours(List<List<TileModel>> map) {
+			// above
+			if (gridPosition.y > 0) {
+				Vector2 n = new Vector2(gridPosition.x, gridPosition.y - 1);
+				neighbours.Add(map[(int)n.x][(int)n.y]);
+			}
+
+			// below
+			if (gridPosition.y < map.Count - 1) {
+				Vector2 n = new Vector2(gridPosition.x, gridPosition.y + 1);
+				neighbours.Add(map[(int)n.x][(int)n.y]);
+			}
+
+			// left
+			if (gridPosition.x > 0) {
+				Vector2 n = new Vector2(gridPosition.x - 1, gridPosition.y);
+				neighbours.Add(map[(int)n.x][(int)n.y]);
+			}
+
+			// right
+			if (gridPosition.x < map.Count - 1) {
+				Vector2 n = new Vector2(gridPosition.x + 1, gridPosition.y);
+				neighbours.Add(map[(int)n.x][(int)n.y]);
+			}
+		}
+	}
+}
